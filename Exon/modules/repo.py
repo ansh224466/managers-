fd7232d28d3c4a7c569c0e58c52c21a4805a817e
ZADKIEL <116737841+Moca96@users.noreply.github.com>
2023-09-18 03:06:36 -0700

    Create repo.py
    #update

---
+++ Exon/modules/repo.py
@@ -0,0 +1,42 @@
+# ""DEAR PRO PEOPLE,  DON'T REMOVE & CHANGE THIS LINE
+# TG :- @Abishnoi1M
+#     MY ALL BOTS :- Abishnoi_bots
+#     GITHUB :- KingAbishnoi ""
+
+from platform import python_version as y
+
+from pyrogram import __version__ as z
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import __version__ as o
+from telethon import __version__ as s
+
+from Exon import Abishnoi as pbot
+
+ABISHNOIX = "https://te.legra.ph/file/abfc49a1cc4b5629dc8cd.jpg"
+
+
+@pbot.on_cmd("repo")
+async def repo(_, message):
+    await message.reply_photo(
+        photo=ABISHNOIX,
+        caption=f"""✨ **ʜᴇʏ {message.from_user.mention},**
+
+**ᴏᴡɴᴇʀ  : [Zadkiel](https://t.me/zadkiel_x)**
+**ᴘʏᴛʜᴏɴ ᴠᴇʀꜱɪᴏɴ :** `{y()}`
+**ʟɪʙʀᴀʀʏ ᴠᴇʀꜱɪᴏɴ :** `{o}`
+**ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀꜱɪᴏɴ :** `{s}`
+**ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀꜱɪᴏɴ :** `{z}`
+""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "•ᴍᴜꜱɪᴄ•", url="https://github.com/Moca96/Anonxmusic"
+                    ),
+                    InlineKeyboardButton(
+                        "•ʀᴏʙᴏᴠ1•", url="https://t.me/Network_Zadkiel/451"
+                    ),
+                ]
+            ]
+        ),
+    )
